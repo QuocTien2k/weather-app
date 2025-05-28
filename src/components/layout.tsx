a9655dacb4a2b2949c9b13c5130b82cad2458443
QuocTien2k <hadesdqt@gmail.com>
2025-05-28 16:25:50 +0700

    create + custom component Header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,10 @@
 import type { PropsWithChildren } from "react";
+import Header from "./Header";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="bg-gradient-to-br from-background to-muted">
-      <header></header>
+      <Header />
       <main className="min-h-screen continer mx-auto px-4 py-8">
         {children}
       </main>
