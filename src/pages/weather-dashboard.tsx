5f184c015eaca976fe5a60335c673841949e8b87
QuocTien2k <hadesdqt@gmail.com>
2025-05-29 11:11:05 +0700

    custom UI weather-dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { RefreshCw } from "lucide-react";
 import React from "react";
 
 const WeatherDashboard = () => {
-  return <div>WeatherDashboard</div>;
+  return (
+    <>
+      <div>
+        {/* Favorive Cities */}
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl font-bold tracking-tight">My location</h1>
+          <Button variant="outline" size="icon">
+            <RefreshCw className="h4 w-4" />
+          </Button>
+        </div>
+
+        {/*Current and Hourly weather */}
+      </div>
+    </>
+  );
 };
 
 export default WeatherDashboard;
